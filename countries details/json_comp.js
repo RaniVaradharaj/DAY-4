f81ab120ee9a3de37a72310837d91b502bf62144
RaniVaradharaj <ranidvhsr31@gmail.com>
2022-02-02 16:30:12 +0530

    countries

--- countries details/json_comp.js
+++ countries details/json_comp.js
@@ -1,5 +1,5 @@
 var obj1 = { name: "Person 1", age:5}; 
-var obj2 = { age:5, name: "Person 1",color:"red"}; 
+var obj2 = { age:5, name: "Person 1"}; 
 var same=true;
 if(Object.keys(obj1).length==Object.keys(obj2).length){
     for(key in obj1) { 

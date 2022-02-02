d0df7a42da2f11e26709e1ceb685f79642afd602
RaniVaradharaj <ranidvhsr31@gmail.com>
2022-02-02 09:35:36 +0530

    Json comp

--- countries details/json_comp.js
+++ countries details/json_comp.js
@@ -1,12 +1,11 @@
-var obj1 = { name: "Person 1", age:5 };
-var obj2 = { age:5, name: "Person 1" };
-
- //  console.log(Object.keys(obj1));
+var obj1 = { name: "Person 1", age:5}; 
+var obj2 = { age:5, name: "Person 1",color:"red"}; 
 var same=true;
 if(Object.keys(obj1).length==Object.keys(obj2).length){
     for(key in obj1) { 
+
         if(obj1[key] == obj2[key]) {
-            continue;
+            continue; 
         }
         else {
             same=false;
